97a245a7b9661199fa0244a9c0cbbef2075ce737
ozywuli <ozy.wu.li@gmail.com>
2015-10-15 01:07:26 -0700

    masonry infinite scroll imagesloaded

--- gulpfile.js
+++ gulpfile.js
@@ -8,13 +8,16 @@ var autoprefixer = require('gulp-autoprefixer');
 var cmq = require('gulp-combine-media-queries');
 var minifycss = require('gulp-minify-css');
 
+var browserify = require('browserify');
+var source = require('vinyl-source-stream');
+var concat = require('gulp-concat');
 var uglify = require('gulp-uglify');
 
 var plumber = require('gulp-plumber');
 var rename = require('gulp-rename');
 var clipboard = require('gulp-clipboard');
 
-gulp.task('html', function() {
+gulp.task('html', ['css', 'js'], function() {
   return compileHTML();
 });
 gulp.task('css', function() {
@@ -25,8 +28,13 @@ gulp.task('js', function() {
 })
 
 
-
-
+/*gulp.task('test', function() {
+  return gulp.src('build/main.css')
+    .pipe(rename({ suffix: '.min' }))
+    .pipe(minifycss())
+    .pipe(gulp.dest('build/'))
+})
+*/
 function compileHTML() {
   var returnObj = gulp.src('theme/templates/main.tumblr')
     .pipe(preprocess())
@@ -58,6 +66,13 @@ function compileCSS() {
 
 
 function compileJS() {
+/*  return browserify('./theme/js/main.js')
+      .bundle()
+      .on('error', watchTask) // restart watch task on error
+      //Pass desired output filename to vinyl-source-stream
+      .pipe(source('main.js'))
+      .pipe(concat('./build/main')).js
+      .pipe(gulp.dest('./build'))*/
   return gulp.src('theme/js/main.js')
     .pipe(gulp.dest('build/'))
     .pipe(rename({ suffix: '.min' }))
@@ -86,12 +101,12 @@ function watchHTML(error) {
 
 function watchCSS(error) {
     handleError(error);
-    gulp.watch(['theme/sass/*.scss', 'theme/sass/**/*.scss'], ['css']);
+    gulp.watch(['theme/sass/*.scss', 'theme/sass/**/*.scss'], ['html']);
 }
 
 function watchJS(error) {
     handleError(error);
-    gulp.watch(['theme/js/main.js'], ['js']);
+    gulp.watch(['theme/js/main.js'], ['html']);
 }
 
 
--- theme/js/main.js
+++ theme/js/main.js
@@ -1,1 +1,60 @@
-var x = 'hello';
+(function() {
+
+  // Main content container
+  var $container = $('.content');
+
+  // Masonry + ImagesLoaded
+  $container.imagesLoaded(function(){
+    $container.masonry({
+      // selector for entry content
+      itemSelector: '.post',
+      columnWidth: 200
+    });
+  });
+
+  // Infinite Scroll
+  $container.infinitescroll({
+
+    // selector for the paged navigation (it will be hidden)
+    navSelector  : ".pagination",
+    // selector for the NEXT link (to page 2)
+    nextSelector : ".pagination__next",
+    // selector for all items you'll retrieve
+    itemSelector : ".post",
+
+    // finished message
+    loading: {
+      finishedMsg: 'No more pages to load.'
+      }
+    },
+
+    // Trigger Masonry as a callback
+    function( newElements ) {
+      // hide new items while they are loading
+      var $newElems = $( newElements ).css({ opacity: 0 });
+      // ensure that images load before adding to masonry layout
+      $newElems.imagesLoaded(function(){
+        // show elems now they're ready
+        $newElems.animate({ opacity: 1 });
+        $container.masonry( 'appended', $newElems, true );
+      });
+
+
+
+  });
+  
+  /**
+   * OPTIONAL!
+   * Load new pages by clicking a link
+   */
+
+  // Pause Infinite Scroll
+/*  $(window).unbind('.infscr');
+
+  // Resume Infinite Scroll
+  $('.pagintion__prev').click(function(){
+    $container.infinitescroll('retrieve');
+    return false;
+  });*/
+
+})();
